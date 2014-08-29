2c889042ea6db817e7a4ba56e76859e5eb09f3b8
Rick Metzger <rickymetz@gmail.com>
2014-08-29 19:08:40 -0400

    Added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #set to actual hostname
+  # required for heroku
+  config.action_mailer.default_url_options = { :host => 'http://murmuring-ocean-1520.herokuapp.com/' }
+
 end
